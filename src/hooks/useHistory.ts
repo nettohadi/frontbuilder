05f8f7825354ba58d1c0f5c60468141a01eadef3
nettohadi <32300655+nettohadi@users.noreply.github.com>
2022-11-05 16:59:48 +0800

    chore: remove unused 'useState' import

--- src/hooks/useHistory.ts
+++ src/hooks/useHistory.ts
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import history from '@src/global/history';
 import PageData from '@src/context';
 import { useRender } from '@src/hooks/index';
