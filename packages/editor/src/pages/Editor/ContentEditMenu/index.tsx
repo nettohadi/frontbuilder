ca7d50ca514ab1f52c3bed850bf831669ecf8e24
nettohadi <32300655+nettohadi@users.noreply.github.com>
2022-12-19 20:21:53 +0800

    feat: initial work

--- packages/editor/src/pages/Editor/ContentEditMenu/index.tsx
+++ packages/editor/src/pages/Editor/ContentEditMenu/index.tsx
@@ -164,7 +164,7 @@ const ContentEditMenu = ({ visible }: { visible: boolean }) => {
   };
 
   return (
-    <FloatingMenu content={<Content />} visible={visible}>
+    <FloatingMenu content={<Content />} visible={visible} showArrow>
       <S.Placer />
     </FloatingMenu>
   );
--- packages/editor/src/pages/Editor/Pages/index.tsx
+++ packages/editor/src/pages/Editor/Pages/index.tsx
@@ -1,48 +1,34 @@
-import { HeadingContainer } from '@src/pages/Editor/shared';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { FiMoreVertical } from 'react-icons/fi';
+import { SlHome } from 'react-icons/sl';
+
+import usePages from '@src/hooks/queries/usePages';
 import * as G from '@src/styles';
 import * as S from './styles';
-import { BiPlus } from 'react-icons/bi';
-import { FiMoreVertical } from 'react-icons/fi';
-import React from 'react';
-import { BsCheck2 } from 'react-icons/bs';
+import { HeadingContainer } from '@src/pages/Editor/shared';
 
 const Pages = () => {
+  const params = useParams<{ websiteId: string; pageId: string }>();
+  const { isLoading, data } = usePages(Number(params.websiteId));
   return (
     <div>
       <HeadingContainer>Pages</HeadingContainer>
       <G.Divider color={'dark'} />
       <S.PagesContainer>
-        <S.PageItem>
-          <div>
-            <S.IconBox>
-              <BsCheck2 />
-            </S.IconBox>
-            Home
-          </div>
-          <FiMoreVertical size="14px" cursor="pointer" />
-        </S.PageItem>
-        <G.Divider color={'light'} />
-        <S.PageItem>
-          <div>
-            <S.IconBox />
-            About
-          </div>
-          <FiMoreVertical size="14px" cursor="pointer" />
-        </S.PageItem>
-        <G.Divider color={'light'} />
-        <S.PageItem>
-          <div>
-            <S.IconBox />
-            F.A.Q
-          </div>
-          <FiMoreVertical size="14px" cursor="pointer" />
-        </S.PageItem>
-        <G.Divider color={'light'} />
-        <S.PageItem>
-          <div>
-            <BiPlus /> Create New Page
-          </div>
-        </S.PageItem>
+        {!isLoading &&
+          data?.map((page) => (
+            <>
+              <S.PageItem>
+                <div className="check-mark">
+                  {page.isDefault && <SlHome size={13} />}
+                </div>
+                <div>{page.name}</div>
+                <FiMoreVertical size="14px" cursor="pointer" />
+              </S.PageItem>
+              <G.Divider color={'light'} />
+            </>
+          ))}
       </S.PagesContainer>
     </div>
   );
