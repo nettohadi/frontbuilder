90f9e4d1cf3ef60945ac057cea26e5e53462b408
nettohadi <32300655+nettohadi@users.noreply.github.com>
2023-01-06 22:55:22 +0800

    chore: remove unused import

--- packages/editor/src/pages/Editor/User/index.tsx
+++ packages/editor/src/pages/Editor/User/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 
 import FloatingMenu from '@src/components/FloatingMenu';
 import * as G from '@src/styles';
